854c3557c96d3de7168537d48a6f24b9de1ac8e8
Dayton Johnson <daytonrjohnson@gmail.com>
2017-03-11 18:58:25 -0600

    Switched to https for quote request
    To fix mixed content error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var apiURL = "http://quotesondesign.com/wp-json/posts" + "?filter[orderby]=rand" + "&filter[posts_per_page]=" + "&callback=?";
+var apiURL = "https://quotesondesign.com/wp-json/posts" + "?filter[orderby]=rand" + "&filter[posts_per_page]=" + "&callback=?";
 
 var currentQuote = "";
 var currentName = "";
